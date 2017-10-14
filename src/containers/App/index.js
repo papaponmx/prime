58b96aa279c8857be9e0340a6aa16b3315801919
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-10-13 22:49:58 -0500

    Installed redux-form and added it to reducer;

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../Home';
 import About from '../About';
+import AddRole from '../Add-Role';
 
 const App = () => (
   <div>
     <header>
       <Link to="/">Home</Link>
       <Link to="/about-us">About</Link>
+      <Link to="/add-role">Add Role</Link>
     </header>
 
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
+      <Route exact path="/add-role" component={AddRole}> </Route>
     </main>
   </div>
-)
+);
 
-export default App
+export default App;
--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import { routerMiddleware } from 'react-router-redux'
-import thunk from 'redux-thunk'
-import createHistory from 'history/createBrowserHistory'
-import rootReducer from './modules'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { routerMiddleware } from 'react-router-redux';
+import { reducer as formReducer } from 'redux-form'
+import thunk from 'redux-thunk';
+import createHistory from 'history/createBrowserHistory';
+import rootReducer from './modules';
 
-export const history = createHistory()
 
-const initialState = {}
-const enhancers = []
+export const history = createHistory();
+
+const initialState = {};
+const enhancers = [];
 const middleware = [
   thunk,
   routerMiddleware(history)
@@ -29,6 +31,7 @@ const composedEnhancers = compose(
 const store = createStore(
   rootReducer,
   initialState,
+  form: formReducer,
   composedEnhancers
 );
 
