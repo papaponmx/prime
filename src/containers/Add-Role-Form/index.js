ac4da6705bdef3d4cb5402bc20d882dbe4f8a4f6
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-10-13 22:55:31 -0500

    Renamed folder and implemented form.

---
+++ src/containers/Add-Role-Form/index.js
@@ -0,0 +1,30 @@
+import React, {PropTypes} from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+
+let AddRoleForm = props => {
+  const { handleSubmit } = props
+  return (
+    <form onSubmit={ handleSubmit }>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  };
+
+  AddRoleForm = reduxForm({
+    // a unique name for the form
+    form: 'add-role-form'
+  })(AddRoleForm);
+
+  export default AddRoleForm;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../Home';
 import About from '../About';
-import AddRole from '../Add-Role';
+import AddRoleForm from '../Add-Role-Form';
 
 const App = () => (
   <div>
@@ -15,7 +15,7 @@ const App = () => (
     <main>
       <Route exact path="/" component={Home} />
       <Route exact path="/about-us" component={About} />
-      <Route exact path="/add-role" component={AddRole}> </Route>
+      <Route exact path="/add-role" component={AddRoleForm}> </Route>
     </main>
   </div>
 );
