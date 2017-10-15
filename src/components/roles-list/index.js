fe820b01f507d23ad4284b832a85590146162071
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-10-15 00:31:28 -0500

    Created roles lists component.

---
+++ src/components/roles-list/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+// TODO: Pass roles to this component as props
+// TODO: Render list of roles
+
+export default class rolesList extends Component {
+
+  render() {
+    return (
+      <div className="class-name">
+        content
+      </div>
+    );
+  }
+}
