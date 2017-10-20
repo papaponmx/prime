5903b07733be18357f784e32809de80f382861a1
Jaime Rios <jaime.madrigal@amk-technologies.com>
2017-10-20 18:02:34 -0500

    Changed Add-roles to Roles.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,20 +2,20 @@ import { Route, Link, Switch } from 'react-router-dom';
 import React from 'react';
 import Home from '../Home';
 import About from '../About';
-import AddRolePage from '../Add-Role-Page';
+import RolePage from '../Roles';
 
 const App = () => (
     <div>
       <header>
         <Link to="/">Home</Link>
         <Link to="/about-us">About</Link>
-        <Link to="/add-role">Add Role</Link>
+        <Link to="/roles">Roles</Link>
       </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about-us" component={About} />
-            <Route path="/add-role" component={AddRolePage} />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about-us" component={About} />
+          <Route path="/roles" component={RolePage} />
           </Switch>
         </main>
     </div>
