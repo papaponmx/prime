489e10a7415a58ae860e863a7a79a28c7a43e28f
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-11-20 15:54:30 -0600

    Added material design form.

--- src/components/Add-Role-Form/index.js
+++ src/components/Add-Role-Form/index.js
@@ -1,15 +1,27 @@
 import React from 'react'
 // import { connect } from 'react-redux'
 import { Field, reduxForm, } from 'redux-form'
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
+
+
+const renderField = props => {
+  return (
+    <TextField placeholder="Daughter, student, boss, etc..."
+    id={props.id}
+    />
+   )
+}
 
 class AddRoleForm extends React.Component {
 
   render() {
     return (
-      <form>
-        <label htmlFor="add-role-input">Add Role:</label>
-        <Field name="add-role-input" component="input" type="text" id="add-role-input"/>
+      <form className="flex-col-start" >
+        <label id="add-role-input">New Role</label>
+        <Field name="add-role-input" component={renderField} id="add-role-input"/>
+        <Button className="cta" label="Default">Default</Button>
       </form>
     )
   }
