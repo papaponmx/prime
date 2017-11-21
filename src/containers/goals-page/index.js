9fb53ebf6b960ff83cea5d17edaa790bc2389f44
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-11-20 18:50:58 -0600

    Connected goals page to redux.

--- src/containers/goals-page/index.js
+++ src/containers/goals-page/index.js
@@ -1,11 +1,29 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 import React, { Component } from 'react'
+import GoalsList from '../../components/goals-list';
+import Button from 'material-ui/Button';
 
-export default class GoalsPage extends Component {
+class GoalsPage extends Component {
   render() {
     return (
       <div>
-        
+      <h1>Goals</h1>
+      <GoalsList goals={this.props.goals} />
+      <Button onClick={ () => this.props.changePage()}>Add Goal</Button>
       </div>
     )
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    changePage: () => push('/goals/add')
+}, dispatch)
+
+function mapStateToProps(state) {
+    return { goals: state.goals };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalsPage);
+
