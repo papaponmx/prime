1a410d4d00188e53ca11485a002e1cd5f1aea2e2
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-11-20 18:51:09 -0600

    Included Add goal form.

--- src/containers/add-goals-page/index.js
+++ src/containers/add-goals-page/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import AddGoalForm from '../../components/add-goal-form';
 
 export default class AddGoalsPage extends Component {
   render() {
     return (
       <div>
         <h1>Add Goals Page</h1>
+        <AddGoalForm />
       </div>
     )
   }
