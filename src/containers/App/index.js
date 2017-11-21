bfc40b3c3aef927ea5639ec401e43210e99592a7
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-11-20 18:51:50 -0600

    Included goalls link.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -19,7 +19,7 @@ const App = () => (
         <Link to="/">Home</Link>
         <Link to="/about-us">About</Link>
         <Link to="/roles">Roles</Link>
-        <Link to="/roles/add">Add Role</Link>
+        <Link to="/goals">Goles</Link>
       </Typography>
       <IconButton color="contrast" aria-label="Menu">
         <MenuIcon />
