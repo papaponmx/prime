312b8ef84499c8a0c205fc71ce64a47e98410b20
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-11-20 19:19:52 -0600

    Added Placeholder content

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => (
   <div>
     <h1>About Page</h1>
-    <p>Did you get here via Redux?</p>
+    <p>TODO: Create real content for this page.</p>
   </div>
 )
 
