787bed5c15e99150c0db6d2ba1e63c4874306feb
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-01-07 16:35:19 -0600

    Improved layout.

--- src/components/goals-list/index.js
+++ src/components/goals-list/index.js
@@ -1,17 +1,20 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 
 export default class GoalsList extends Component {
-    renderList() {
-        return _.map(this.props.goals, goal => {
-            return <li key={goal.id}>{goal.name}</li>
-        })
-    }
+  renderList() {
+    return _.map(this.props.goals, goal => {
+      return <ListItem key={goal.id}>
+        <ListItemText primary={goal.name} />
+      </ListItem>
+    })
+  }
 
-    render() {
-        return (
-            <ul className="goals-list">
-              {this.props.goals ? this.renderList() : ''}
-            </ul>);
+  render() {
+    return (
+      <List className="goals-list">
+        {this.props.goals ? this.renderList() : ''}
+      </List>);
     }
-}
+  }
--- src/components/roles-list/index.js
+++ src/components/roles-list/index.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import Collapse from 'material-ui/transitions/Collapse';
+import IconButton from 'material-ui/IconButton';
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 
 import GoalsList from '../goals-list';
 
 export default class RolesList extends Component {
   renderList() {
-      return _.map(this.props.roles, role => {
-        return (
-        <li key={role.id}>
-          {role.name}
-          <GoalsList goals={role.goals} />
-        </li>
-      )
-    });
-  }
+    return _.map(this.props.roles, role => {
+      return (
+        [
+          <ListItem
+            key={role.id + 'B'}
+          >
+            <div className="flex-row-space-between">
+              {role.name}
+              <IconButton
+                onClick={this.handleExpandClick}
+                // aria-expanded={this.state.expanded}
+                aria-label="Show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            </div>
+                <Collapse>
+                  <GoalsList goals={role.goals} />
+                </Collapse>
+              </ListItem>,
+              <Divider key={role.id + 'A'} light />
+            ]
+          )
+        });
+      }
 
-  render() {
-    return (
-      <ul className="roles-list">
-        {this.props.roles ? this.renderList() : '' }
-      </ul>);
-    }
-  }
+      render() {
+        return (
+          <List className="roles-list">
+            {this.props.roles ? this.renderList() : '' }
+          </List>);
+        }
+      }
