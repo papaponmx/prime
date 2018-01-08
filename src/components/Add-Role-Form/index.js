4fc529e490ce61fb94cecc4455b10852b89353d7
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-01-07 20:22:17 -0600

    Added textField.

--- src/components/Add-Role-Form/index.js
+++ src/components/Add-Role-Form/index.js
@@ -2,34 +2,69 @@ import { addRole } from '../../actions';
 import React from 'react'
 import { connect } from 'react-redux';
 import { Field, reduxForm, } from 'redux-form'
-// import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import { reset } from 'redux-form';
+import TextField from 'material-ui/TextField';
 
-// const renderField = props => {
-//   // FIXME: When I use this function,  redux-form does not emmit the event, nor catches the input value.
-//   return (
-//     <TextField name={props.name} placeholder="Daughter, student, boss, etc..."
-//     id={props.id}
-//     />
-//    )
-// }
+// TODO: Migrate render TextField to another component
+// TODO: Add role description as an optional parameter;
 
-// TODO: Add role description input to the form and to Redux.
-// TODO: Add validations;
+const renderField = ({  input,  label,  type,  meta: { touched, error, }}) => {
+  if (touched && error) {
+    return (
+      <TextField
+        required
+        error
+        id="required"
+        label={error}
+        margin="normal"
+        error
+      />
+    );
+  }
+
+  return (
+    <TextField
+      required
+      id="required"
+      label={label}
+      margin="normal"
+    />
+  )
+}
 
 class AddRoleForm extends React.Component {
+
   render() {
     return (
       <form className="flex-col-start" onSubmit={this.props.handleSubmit}>
-        <label id="add-role-input">New Role</label>
-        <Field name="role" component="input" id="add-role-input"/>
+        <Field
+          name="role"
+          component={renderField}
+          id="add-role-input"
+          label="Add Role"
+
+        />
+        {/* <Field
+          name="role"
+          component={renderField}
+          id="add-role-description-input"
+          label="Role description"
+
+        /> */}
         <Button type="submit" className="cta">Add Role</Button>
+
       </form>
     )
   }
 }
 
+const validate = values => {
+  const errors= {};
+  if (!values.role)
+  errors.role = `Role can't be empty`;
+  return errors;
+}
 
 const mapDispatchToProps = (dispatch) => {
   return {
@@ -43,6 +78,7 @@ const mapDispatchToProps = (dispatch) => {
 
 AddRoleForm = reduxForm({
   form: 'AddRoleForm',
+  validate,
 })(AddRoleForm);
 
 export default AddRoleForm = connect (null, mapDispatchToProps)(AddRoleForm);
