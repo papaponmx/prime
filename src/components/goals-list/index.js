6a55996bb7364a9f12d5788e3d4a4857a8da52bb
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-01-13 20:49:06 -0600

    Updated select props.

--- src/components/goals-list/index.js
+++ src/components/goals-list/index.js
@@ -1,22 +1,35 @@
 import _ from 'lodash';
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import List, { ListItem, ListItemText } from 'material-ui/List';
+import List, { ListItem, } from 'material-ui/List';
 
-export default class GoalsList extends Component {
+class GoalsList extends Component {
+  // TODO: Optimize this component
+  // IDEA: Add role badge to each list item
   renderList() {
-    return _.map(this.props.goals, goal => {
-      return(
-        <ListItem key={goal.id + goal.name} className="flex-row-space-between goal-single">
-          <ListItemText primary={goal.name} />
-        </ListItem>
-      );
+    return _.map(this.props.roles, role => {
+      return _.map(role.goals, goal => <ListItem>{goal.name}</ListItem>);
     });
   }
 
   render() {
+    let goalFinded;
+    if(this.props.roles) {
+      goalFinded = _.find(this.props.roles, role => {
+        if(role.goals){
+          return role.goals.length > 0
+        }
+      });
+    }
     return (
       <List className={"goals-list" + (this.props.nested && ' nested-list')} >
-        {this.props.goals ? this.renderList() : ''}
+        {(this.props.roles &&  goalFinded !== undefined) ? this.renderList() : 'You have no goals defined'}
       </List>);
     }
   }
+
+  const mapStateToProps= state => ({
+    roles: state.roles.list,
+  });
+
+  export default connect(mapStateToProps, null)(GoalsList);
