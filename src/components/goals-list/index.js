89819bb871ede7814e0143b07d8aa3050334ca0a
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-04-08 21:42:41 -0500

    Goals List component refactor.

--- src/components/goals-list/index.js
+++ src/components/goals-list/index.js
@@ -2,17 +2,12 @@ import _ from 'lodash';
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import List, { ListItem, } from 'material-ui/List';
+import { CardText } from 'material-ui/Card';
 
 class GoalsList extends Component {
   // TODO: Optimize this component
   // IDEA: Add role badge to each list item
   renderList() {
-    return _.map(this.props.roles, role => {
-      return _.map(role.goals, goal => <ListItem>{goal.name}</ListItem>);
-    });
-  }
-
-  render() {
     let goalFinded;
     if(this.props.roles) {
       goalFinded = _.find(this.props.roles, role => {
@@ -21,9 +16,25 @@ class GoalsList extends Component {
         }
       });
     }
+    if (this.props.roles &&  goalFinded !== undefined) {
+      return _.map(this.props.roles, role => {
+        return _.map(role.goals, goal => (
+          <ListItem>{goal.name}</ListItem>
+        ))
+      });
+    } else {
+      return (
+        <ListItem>
+          You have no goals defined for this role
+        </ListItem>
+      )
+    }
+  }
+
+  render() {
     return (
       <List className={"goals-list" + (this.props.nested && ' nested-list')} >
-        {(this.props.roles &&  goalFinded !== undefined) ? this.renderList() : 'You have no goals defined'}
+        {this.renderList()}
       </List>);
     }
   }
