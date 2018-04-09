71e7add3bbd004a3b7a003caeb02884b774d0785
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-04-08 20:45:03 -0500

    Improved layout

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { CardHeader } from 'material-ui/Card';
+
 import RolesList from '../../components/roles-list';
 import GoalsList from '../../components/goals-list';
 
@@ -10,8 +12,19 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Home</h1>
+        <CardHeader
+          title="Prime"
+        />
+        <CardHeader
+          title="Roles"
+          subtitle="Subtitle"
+        />
         <RolesList roles={this.props.roles ? this.props.roles : []} />
+
+        <CardHeader
+          title="Goals"
+          subtitle="Here are the next things you are about to accomplish"
+        />
         <GoalsList goals={this.props.goals ? this.props.goals : []} />
       </div>
     )
