632378da66ef3d1260e20529ee1a81e243a185fa
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-04 23:15:21 -0500

    Improved signin page.

--- src/containers/signup-page/index.js
+++ src/containers/signup-page/index.js
@@ -1,1 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+export class SignupPage extends Component {
+
+  render() {
+    return (
+      <div>
+        SignupPage
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignupPage);
