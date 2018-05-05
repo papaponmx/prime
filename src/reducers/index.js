49afe57392b15812e6390a68a3d945444756c78d
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-05 15:36:22 -0500

    Added firebase reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import rolesReducer from './reducer_roles';
-import collapseReducer from './reducer_collapse';
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import rolesReducer from './reducer_roles'
+import collapseReducer from './reducer_collapse'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
   roles: rolesReducer,
   form: formReducer,
   collapses: collapseReducer,
-});
+  firebase: firebaseReducer,
+})
 
-export default rootReducer;
+export default rootReducer
