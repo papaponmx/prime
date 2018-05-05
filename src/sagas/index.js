2de861daee9ad54b4865d0b4a7a2ea5e802b1160
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-05 15:27:01 -0500

    Added redux-saga.

---
+++ src/sagas/index.js
@@ -0,0 +1,27 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
+
+// worker Saga: will be fired on USER_FETCH_REQUESTED actions
+function* fetchUser(action) {
+  yield console.log('waddup');
+}
+
+/*
+  Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
+  Allows concurrent fetches of user.
+*/
+function* mySaga() {
+  yield takeEvery("USER_FETCH_REQUESTED", fetchUser);
+}
+
+/*
+  Alternatively you may use takeLatest.
+
+  Does not allow concurrent fetches of user. If "USER_FETCH_REQUESTED" gets
+  dispatched while a fetch is already pending, that pending fetch is cancelled
+  and only the latest one will be run.
+*/
+function* mySaga() {
+  yield takeLatest("USER_FETCH_REQUESTED", fetchUser);
+}
+
+export default mySaga;
--- src/store.js
+++ src/store.js
@@ -3,11 +3,20 @@ import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import rootReducer from './reducers';
 
+import firebase from 'firebase';
+import { firebase as fbConfig, reduxFirebase as reduxConfig } from './settings/firebase';
+import mySaga from './sagas';
+import createSagaMiddleware from 'redux-saga';
+
+
 export const history = createHistory();
 const initialState = {};
 const enhancers = [];
+const sagaMiddleware = createSagaMiddleware();
+
 const middleware = [
-  routerMiddleware(history)
+  routerMiddleware(history),
+  sagaMiddleware.run(mySaga),
 ];
 
 
