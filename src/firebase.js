5843a462998380823a78126df3ed6adddc57a6a6
Jaime Rios <jaimemadrigalrios@gmail.com>
2018-05-13 11:29:39 -0500

    Removed duplicated file

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-export default = {
-  apiKey: 'AIzaSyCgKm916Y4RfpSl_hpQQMeq38w_HMSjbq0',
-  authDomain: 'prime-bc289.firebaseapp.com',
-  databaseURL: 'https://prime-bc289.firebaseio.com',
-  projectId: 'prime-bc289',
-  storageBucket: 'prime-bc289.appspot.com',
-  messagingSenderId: '797840900299',
-}
