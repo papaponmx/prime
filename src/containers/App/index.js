cad648be411ea0c0e1ed2d054d4b01cc66bbde70
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-07-15 14:58:46 -0500

    Added HomePage and user info to it

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,44 +1,50 @@
-import { Link, Route, Switch } from 'react-router-dom';
-
-import About from '../About';
-import AppBar from 'material-ui/AppBar';
-import React from 'react';
-import Loadable from 'react-loadable';
-import SigninPage from '../signin-page';
-import SignupPage from '../signup-page';
-import ic_face_black_24px from '../../icons/ic_face_black_24px.svg';
-import ic_grade_black_24px from '../../icons/ic_grade_black_24px.svg';
-import ic_home_white_24px from '../../icons/ic_apps_black_24px.svg';
-import RequireAuth  from '../auth';
-import loading from '../../components/Loading';
+import AppBar from "material-ui/AppBar";
+import React from "react";
+import Loadable from "react-loadable";
+import { Link, Route, Switch } from "react-router-dom";
+import loading from "../../components/Loading";
+import ic_home_white_24px from "../../icons/ic_apps_black_24px.svg";
+import ic_face_black_24px from "../../icons/ic_face_black_24px.svg";
+import ic_grade_black_24px from "../../icons/ic_grade_black_24px.svg";
+import About from "../About";
+import RequireAuth from "../auth";
+import HomePage from "../Home";
+import SigninPage from "../signin-page";
+import SignupPage from "../signup-page";
 
 const RolePage = Loadable({
-  loader: () => import ('../Roles/index'),
-  loading,
+  loader: () => import("../Roles/index"),
+  loading
 });
 
 const GoalsPage = Loadable({
-  loader: () => import('../goals-page'),
-  loading,
+  loader: () => import("../goals-page"),
+  loading
 });
 
 const AddGoalsPage = Loadable({
-  loader: () => import('../add-goals-page'),
-  loading,
+  loader: () => import("../add-goals-page"),
+  loading
 });
 
 const AddRolesPage = Loadable({
-  loader: () => import('../Add-Roles-Page'),
-  loading,
+  loader: () => import("../Add-Roles-Page"),
+  loading
 });
 
 const App = () => (
   <div>
     <AppBar id="app-navigation">
-      <Link to="/"><img src={ic_home_white_24px} alt="home icon" /></Link>
+      <Link to="/">
+        <img src={ic_home_white_24px} alt="home icon" />
+      </Link>
       {/* <Link to="/about-us">About</Link> TODO: Find a place to put this link */}
-      <Link to="/roles"><img src={ic_face_black_24px} alt="roles icon" /></Link>
-      <Link to="/goals"><img src={ic_grade_black_24px} alt="goals icon" /></Link>
+      <Link to="/roles">
+        <img src={ic_face_black_24px} alt="roles icon" />
+      </Link>
+      <Link to="/goals">
+        <img src={ic_grade_black_24px} alt="goals icon" />
+      </Link>
     </AppBar>
     <main>
       <Switch>
@@ -49,7 +55,7 @@ const App = () => (
         <Route exact path="/roles" component={RequireAuth(RolePage)} />
         <Route exact path="/signin" component={SigninPage} />
         <Route exact path="/signup" component={SignupPage} />
-        <Route exact path="/" component={SigninPage} />
+        <Route exact path="/" component={RequireAuth(HomePage)} />
       </Switch>
     </main>
   </div>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,39 +1,25 @@
-import React, { Component } from 'react';
-import { push } from 'react-router-redux';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { CardHeader } from 'material-ui/Card';
+import React, { Component } from 'react'
+import { push } from 'react-router-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { CardHeader } from 'material-ui/Card'
 
-import RolesList from '../../components/roles-list';
-import GoalsList from '../../components/goals-list';
+import RolesList from '../../components/roles-list'
+import GoalsList from '../../components/goals-list'
 
 class Home extends Component {
 
-  render() {
+  render () {
     return (
       <div>
-        <CardHeader
-          title="Prime"
-        />
-        <CardHeader
-          title="Roles"
-          subtitle="Subtitle"
-        />
-        <RolesList roles={this.props.roles ? this.props.roles : []} />
-
-        <CardHeader
-          title="Goals"
-          subtitle="Here are the next things you are about to accomplish"
-        />
-        <GoalsList goals={this.props.goals ? this.props.goals : []} />
+        <CardHeader title={`Hello ${this.props.userInfo.displayName}`} />
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  goals: state.goals,
-  roles: state.roles,
+  userInfo: state.user.user.information
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
