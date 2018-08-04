503c4c18087829696c8889c9869da2e75bec98a6
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-04 09:41:14 -0500

    Added yarn add @babel/runtime@7.0.0-beta.55 --exact

--- src/components/AddGoalForm/index.js
+++ src/components/AddGoalForm/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
-import { Field, reduxForm, reset} from 'redux-form'
-import { connect } from 'react-redux';
-import Button from 'material-ui/Button';
+import { Field, reduxForm, reset } from 'redux-form'
+import { connect } from 'react-redux'
+import Button from '@material-ui/core/Button'
 
-import { addGoal }from '../../actions';
-// import { renderSelect } from '../form-utils';
+import { addGoal } from '../../actions'
+// import { renderSelect } from '../form-utils'
 
 class AddGoalForm extends Component {
-  render() {
-    // TODO: Rethink goals-roles graph;
+  render () {
+    // TODO: Rethink goals-roles graph
     // Should be something like this:
     // [
     //   {
@@ -20,45 +20,42 @@ class AddGoalForm extends Component {
     // ]
 
     return (
-      <form className="flex-col-start" onSubmit={this.props.handleSubmit}>
-      <label id="add-goal-input">New goal</label>
-      <Field
-      name="goal"
-      component="input"
-      id="add-goal-input"
-      />
-      <Field
-      name=''
-      component='input'
-      id='goal-due-date'
-      type='date'
-      />
-
-      {/* TODO: Add meta-data for goals, like: status = completed, dueDate, priority, etc. */}
-      <Button type="submit" className="cta">Add goal</Button>
+      <form className='flex-col-start' onSubmit={this.props.handleSubmit}>
+        <label id='add-goal-input'>
+          New goal
+        </label>
+        <Field name='goal' component='input' id='add-goal-input' />
+        <Field
+          name=''
+          component='input'
+          id='goal-due-date'
+          type='date' />
+        {/* TODO: Add meta-data for goals, like: status = completed, dueDate, priority, etc. */}
+        <Button type='submit' className='cta'>
+          Add goal
+        </Button>
       </form>
-    );
+    )
   }
 }
 
 // TODO: Create added goal notification success or shit.
 
-const mapStateToProps= state => ({
-  roles: state.roles.list,
-});
-
+const mapStateToProps = state => ({
+  roles: state.roles.list
+})
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onSubmit: (values) => {
-      dispatch(addGoal(values));
-      dispatch(reset('AddGoalForm'));
-    },
+      dispatch(addGoal(values))
+      dispatch(reset('AddGoalForm'))
+    }
   }
 }
 
 AddGoalForm = reduxForm({
-  form: 'AddGoalForm',
-})(AddGoalForm);
+  form: 'AddGoalForm'
+})(AddGoalForm)
 
-export default AddGoalForm = connect(mapStateToProps, mapDispatchToProps) (AddGoalForm);
+export default AddGoalForm = connect(mapStateToProps, mapDispatchToProps)(AddGoalForm)
