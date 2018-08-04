13dee559c248b52ee300bc059d3b0b0c022f2e75
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-04 14:07:23 -0500

    Fresh start

--- src/styles/buttonStyles.js
+++
@@ -1,5 +0,0 @@
-export const themeStyle = {
-  margin: "1em",
-  backgroundColor: '#3f51b5',
-  color: 'white',
-}
--- src/utils/componentAsLoadable.js
+++
@@ -1,8 +0,0 @@
-import loadable from 'react-loadable';
-
-export default (component, route) => {
-    return component = Loadable({
-        loader: () => import(route),
-        loading: Loading,
-    });
-}
