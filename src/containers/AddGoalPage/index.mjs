75c3d9c4b5d31bd2a0ea734a552f2f7fffa5dbf9
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-04 23:30:09 -0500

    Restored AddGoalPage to pure function.

--- src/containers/AddGoalPage/index.mjs
+++ src/containers/AddGoalPage/index.mjs
@@ -1,27 +1,10 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import AddGoalForm from '../../components/AddGoalForm'
-import addGoal from '../../actions';
+import React from 'react'
 
+const AddGoalsPage = (params) => (
+  <section>
+  <h1>Add Goals Page</h1>
+  <AddGoalForm />
+  </section>
+)
 
-class AddGoalsPage extends Component {
-
-  render() {
-
-    return (
-      <section>
-        <h1>Add Goals Page</h1>
-        <AddGoalForm
-          handleSubmit={this.props.addGoal}
-        />
-      </section>
-    );
-  }
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  addGoal: values => dispatch(addGoal(values)),
-})
-
-
-export default connect(null, mapDispatchToProps)(AddGoalsPage)
+export default AddGoalsPage
