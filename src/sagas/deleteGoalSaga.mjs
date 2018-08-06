199d2f34e5f4420dbe9a9ba62c014622b86fcfef
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-06 07:16:20 -0500

    Restored deleteGoalSaga

--- src/sagas/deleteGoalSaga.mjs
+++ src/sagas/deleteGoalSaga.mjs
@@ -1,8 +1,14 @@
+import { database as db, app as firebase } from '../store'
 
-const deleteGoalSaga = function * (action) {
+const deleteGoalSaga = function * ({payload: id}) {
 
+  yield db.collection('users')
+    .doc(firebase.auth().getUid())
+    .collection('goals')
+    .doc(id)
+    .delete()
+    .then(() => '')
+    .catch((error) => error)
 }
-
-
 
 export default deleteGoalSaga
