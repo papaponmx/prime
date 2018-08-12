2d75b9e72d266e46d24a55805e6b1fc473472529
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-12 01:27:15 -0500

    yarn generate LoginPage

---
+++ app/containers/LoginPage/index.js
@@ -0,0 +1,63 @@
+/**
+ *
+ * LoginPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectLoginPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+/* eslint-disable react/prefer-stateless-function */
+export class LoginPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>LoginPage Prime Planner App</title>
+          <meta name="description" content="Login into your potential" />
+        </Helmet>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+LoginPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  loginpage: makeSelectLoginPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+);
+
+const withReducer = injectReducer({ key: 'loginPage', reducer });
+const withSaga = injectSaga({ key: 'loginPage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(LoginPage);
