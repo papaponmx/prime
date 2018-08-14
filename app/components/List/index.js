e5f461147e39bf37218f22f404c29b58d4f9925c
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-13 21:57:43 -0500

    Adjusted list props.

--- app/components/List/index.js
+++ app/components/List/index.js
@@ -6,20 +6,30 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 // import styled from 'styled-components';
 
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
+/* eslint-disable */
 const List = ({ list, listType = null }) => {
   const prop = listType === null ? 'header' : `${listType}Header`;
   return (
     <div>
-      <FormattedMessage {...messages[prop]} />
-      <ul>{list.map(item => <li> {item.name} </li>)}</ul>
+    <h3>
+    <FormattedMessage {...messages[prop]} />
+    </h3>
+    <ul>
+    {list.map(({id, name}) => <li key={id}>
+      <Link to={`goals/${id}`}> {name} </Link>
+      </li>
+    )}
+    </ul>
     </div>
   );
 };
+/* eslint-enable */
 
 List.propTypes = {
   list: PropTypes.array.isRequired,
