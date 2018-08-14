81b3d1058b45ca5180a226119c000b805d2e1fb7
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-13 21:52:29 -0500

    Added fecha util.

---
+++ app/utils/fecha.js
@@ -0,0 +1,17 @@
+const defaultOptions = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'short',
+  day: 'numeric',
+};
+
+// FIXME: add Unit tests and hour support
+// expect milis
+
+const fecha = (milis, locale = 'en-US', options = defaultOptions) => {
+  const d = new Date(milis);
+  return d.toLocaleDateString(locale, options);
+};
+// return 25 Mar 2018 at 23h
+
+export default fecha;
