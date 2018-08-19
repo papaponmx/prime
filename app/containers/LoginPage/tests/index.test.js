4cc0aab71d8f10e53320f584dfabb4a7f7713302
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-19 12:01:49 -0500

    Added LoginPage render test.

--- app/containers/LoginPage/tests/index.test.js
+++ app/containers/LoginPage/tests/index.test.js
@@ -1,10 +1,17 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-
-// import { LoginPage } from '../index';
+import React from 'react';
+import { shallow } from 'enzyme';
+import { LoginPage } from '../index';
 
 describe('<LoginPage />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  it('Should render component', () => {
+    const wrapper = shallow(<LoginPage />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should render Google Button', () => {
+    const wrapper = shallow(<LoginPage />);
+    expect(JSON.stringify(wrapper.props().children, null, 2)).toMatch(
+      'Sign in with Google',
+    );
   });
 });
