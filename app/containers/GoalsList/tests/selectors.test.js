a3c1987b585117b2765e7db57482e8e2535e7631
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-21 08:00:04 -0500

    Added tests to goalList selectors.

--- app/containers/GoalsList/tests/selectors.test.js
+++ app/containers/GoalsList/tests/selectors.test.js
@@ -1,8 +1,39 @@
-// import { fromJS } from 'immutable';
-// import { selectGoalsListDomain } from '../selectors';
+import {
+  selectGoalsListDomain,
+  selectGoalsList,
+  selectGoalById,
+} from '../selectors';
 
 describe('selectGoalsListDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  const goalsList = {
+    list: [
+      {
+        id: 'ABC',
+        name: 'Do the Laundry',
+      },
+    ],
+    isLoaded: true,
+  };
+
+  const globalState = {
+    goalsList,
+  };
+
+  it('Expect to select the GoalListDomain', () => {
+    expect(selectGoalsListDomain(globalState)).toEqual(goalsList);
+  });
+
+  it('Should select goal by ID', () => {
+    const id = 'ABC';
+    const expectedGoal = {
+      id: 'ABC',
+      name: 'Do the Laundry',
+    };
+
+    expect(selectGoalById(globalState, id)).toEqual(expectedGoal);
+  });
+
+  it('Expect to select GoalList', () => {
+    expect(selectGoalsList(globalState)).toEqual(goalsList.list);
   });
 });
