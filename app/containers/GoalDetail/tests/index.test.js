36122f58185d0ac7121e6c5c66a54a9e291060b5
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-08-22 21:48:51 -0500

    Added tests for GoalDetail

--- app/containers/GoalDetail/tests/index.test.js
+++ app/containers/GoalDetail/tests/index.test.js
@@ -1,10 +1,30 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-// import { GoalDetail } from '../index';
+import { GoalDetail } from '../index';
 
 describe('<GoalDetail />', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+  const renderedComponent = shallow(
+    <GoalDetail
+      goal={{
+        name: 'Get Some',
+        createdAt: '1232132',
+        dueDate: '34324343',
+        id: 'sdfsd23',
+        isDone: false,
+      }}
+    />,
+  );
+
+  it('Should have an <h2></h2> tag', () => {
+    expect(renderedComponent.find('h2').length).toBe(1);
+  });
+
+  it('Should render two <p>', () => {
+    expect(renderedComponent.find('p').length).toBe(2);
+  });
+
+  it('Should render a button', () => {
+    expect(renderedComponent.find('a').length).toBe(1);
   });
 });
