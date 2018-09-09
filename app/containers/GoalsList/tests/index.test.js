cdc5a451995b0d0c33a888df530ca1873558e2e1
Jaime Rios <jaimememadrigalrios@gmail.com>
2018-09-08 22:43:33 -0500

    Adjusted goalsList test.

--- app/containers/GoalsList/tests/index.test.js
+++ app/containers/GoalsList/tests/index.test.js
@@ -7,8 +7,9 @@ import { GoalsList } from '../index';
 
 describe('<GoalsList />', () => {
   let store;
+  const mockStore = configureStore();
+
   beforeEach(() => {
-    const mockStore = configureStore();
     store = mockStore({
       goalsList: {
         isLoading: false,
@@ -35,9 +36,28 @@ describe('<GoalsList />', () => {
   });
 
   it('Should render list of goals', () => {
-    console.log(JSON.stringify(store, null, 2));
     const wrapper = shallow(
-      <GoalsList store={store} goals={store.goalsList} />,
+      <GoalsList
+        store={store}
+        goals={{
+          list: [
+            {
+              createdAt: 1533959056310,
+              dueDate: 1534550400000,
+              id: 'goal-.mzir08u',
+              isDone: false,
+              name: 'Do something',
+            },
+            {
+              createdAt: 1533561506443,
+              dueDate: '2018-08-07',
+              id: 'goal-.ppalv8o',
+              isDone: true,
+              name: 'Some goal',
+            },
+          ],
+        }}
+      />,
     );
     expect(wrapper).toMatchSnapshot();
     // expect(wrapper).toEqual(false);
